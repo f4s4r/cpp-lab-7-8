70f3852acd41320264ac608002369ea38cae7d61
f4s4r <sesetimokhin@edu.hse.ru>
2023-12-13 20:19:46 +0300

    add car-class

---
+++ Car.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 4s4r on 13.12.2023.
+//
+
+#include "Car.h"
---
+++ Car.h
@@ -0,0 +1,15 @@
+//
+// Created by 4s4r on 13.12.2023.
+//
+#include "Vehicle.h"
+#ifndef TRANSPORT_CAR_H
+#define TRANSPORT_CAR_H
+
+
+class Car : protected Vehicle{
+private:
+
+};
+
+
+#endif //TRANSPORT_CAR_H
